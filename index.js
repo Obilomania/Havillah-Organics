7f0892ae857ee93325e435e13916fc459c784353
Obinna Ishmael Iloanya <81533820+Obilomania@users.noreply.github.com>
2022-03-22 21:07:00 +0800

    FIrst-Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelector(".links");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function () {
+  console.log("clicked");
+  navLinks.classList.toggle("open");
+  hamburger.classList.toggle("toggle");
+  return true;
+});
